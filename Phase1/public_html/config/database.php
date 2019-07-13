a02cbbdd80f675cdf7e4f16e784496413544c8a3
rahimenaghavi <49605186+rahimenaghavi@users.noreply.github.com>
2019-07-13 17:28:17 +0430

    Create database.php

---
+++ Phase1/public_html/config/database.php
@@ -0,0 +1,31 @@
+  <?php
+class Database{
+ 
+    //specify your own database credentials
+    private $host = "mysql";
+    private $db_name = "e_voting";
+    private $username = "root";
+    private $password = "rootpassword";
+    public $conn;
+    // private $host = "localhost";
+    // private $db_name = "e_voting";
+    // private $username = "root";
+    // private $password = "";
+    // public $conn;
+ 
+    // get the database connection
+    public function getConnection(){
+ 
+        $this->conn = null;
+ 
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+ 
+        return $this->conn;
+    }
+}
+?>
